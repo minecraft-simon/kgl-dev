4db0afd679b503a322f2ca37932dca2ece66f9a9
minecraft-simon <minecraft_simon@gmx.net>
2023-11-28 17:58:12 +0100

    Added Fabric files

---
+++ src/main/kotlin/io/kstud/Kgldevmc.kt
@@ -0,0 +1,15 @@
+package io.kstud
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Kgldevmc : ModInitializer {
+    private val logger = LoggerFactory.getLogger("kgl-dev-mc")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
